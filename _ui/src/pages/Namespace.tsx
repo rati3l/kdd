5a35a899b6dcad4a8a7683cd4db24b01d0827daa
Patrick Erber <ich@perber.at>
2023-01-08 17:42:08 +0100

    wip: display information of namespace

--- _ui/src/pages/Namespace.tsx
+++ _ui/src/pages/Namespace.tsx
@@ -3,6 +3,7 @@ import PageHead from "../components/PageHead"
 import { Box } from "@mui/system";
 import { Alert,  CircularProgress, Snackbar } from "@mui/material";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 type Props = {
     refreshIntervalMS: number;
@@ -11,36 +12,58 @@ type Props = {
 function Namespace(props: Props) {
     const [loading, setLoading] = useState(true)
     const [errorMessage, setErrrorMessage] = useState("")
-    const [data, setData] = useState([])
+    const [namespaceData, setNamespaceData] = useState({})
+    const [workloadsData, setWorkloadsData] = useState([])
 
-    const params = useParams()
-
+    const paramName = useParams()["name"]
     let interval: any = null
 
     useEffect(() => {
         setLoading(true)
 
-        const fetchFunc = () => {
-        }
+        const fetchFunc = (name: string | undefined) => {
+            const getNamespace = async () => {
+                const { data } = await axios.get(`/api/v1/namespace/${name}`, { headers: { Accept: "application/json" } })
+                return data.data
+            }
 
+            getNamespace().then(data => {
+                setNamespaceData(data.namespace)
+            }).catch((error) => {
+                if (axios.isAxiosError(error)) {
+                    console.error("failed to retrieve namespace information", error.message)
+                    setErrrorMessage("failed to retrieve namespace information")
+                } else {
+                    console.error("a unknown error occurred", error)
+                    setErrrorMessage("a unknown error occurred")
+                }
+            }).finally(() => {
+                setLoading(false)
+            })
+        }
+        setLoading(true)
         // fetching data initially
-        fetchFunc()
+        fetchFunc(paramName)
+
         // check if already a interval is configured
         if (interval) {
             clearInterval(interval)
         }
         // configure new interval
-        interval = setInterval(fetchFunc, props.refreshIntervalMS)
+        interval = setInterval(() => {
+            fetchFunc(paramName)
+        }, props.refreshIntervalMS)
+
         return () => {
             clearInterval(interval)
         }
 
-    }, [props.refreshIntervalMS]);
+    }, [props.refreshIntervalMS, paramName]);
 
 
 
     return <React.Fragment>
-        <PageHead title={`Namespace ${params.name}`} />
+        <PageHead title={`Namespace ${paramName}`} />
         <Box>
             {loading ? <CircularProgress color="primary" /> : <Box></Box>}
         </Box>
